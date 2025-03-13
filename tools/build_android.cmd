d585fbd4393469165d99fd7eb19bbfa72100c3df
Raony Fernandes dos Reis <75029692+raonygamer@users.noreply.github.com>
2025-03-13 14:59:21 -0300

    Update

--- tools/build_android.cmd
+++ tools/build_android.cmd
@@ -1,6 +1,6 @@
 @echo off
 echo Building for arm32... &
-cmd /c adt -package -target apk -arch armv7 -storetype pkcs12 -keystore astroflux.p12 -storepass 192837465 Astroflux_arm32.apk application.xml Game.swf icons &
+cmd /c adt -package -target apk -arch armv7 -storetype pkcs12 -keystore afclient.p12 -storepass 192837465 Astroflux_arm32.apk application.xml Preload.swf icons &
 echo Building for arm64... &
-cmd /c adt -package -target apk -arch armv8 -storetype pkcs12 -keystore astroflux.p12 -storepass 192837465 Astroflux_arm64.apk application.xml Game.swf icons &
+cmd /c adt -package -target apk -arch armv8 -storetype pkcs12 -keystore afclient.p12 -storepass 192837465 Astroflux_arm64.apk application.xml Preload.swf icons &
 echo Finished all builds!
